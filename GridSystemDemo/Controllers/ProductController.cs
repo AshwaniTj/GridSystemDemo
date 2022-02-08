5a42b98360ab86da6bc6e9a61525b9f331abcb5b
AshwaniTj <ashwani.tejan56@gmail.com>
2022-02-08 11:50:20 +0530

    Add project files.

---
+++ GridSystemDemo/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using GridSystemDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GridSystemDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+
+        public ProductController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult GeProducts()
+        {
+            var prodt = _context.Products.ToList();
+            return Json(prodt);
+        }
+
+        public IActionResult PostProducts(Product procut)
+        { 
+
+        
+        }
+    }
+}
---
+++ GridSystemDemo/Models/SubCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GridSystemDemo.Models
+{
+    public class SubCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ParentId { get; set; }
+
+    }
+}
